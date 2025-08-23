19b89fa5a629444bb4c99bd5967119c8a28c16a8
Sergey Bogomolov <bogomolovs693@gmail.com>
2025-08-23 21:07:12 +0300

    add unit tests for save order

--- go.mod
+++ go.mod
@@ -11,12 +11,14 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/segmentio/kafka-go v0.4.49
+	github.com/stretchr/testify v1.8.4
 	github.com/swaggo/http-swagger/v2 v2.0.2
 	github.com/swaggo/swag v1.16.6
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
@@ -31,6 +33,8 @@ require (
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/swaggo/files/v2 v2.0.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/mod v0.17.0 // indirect
@@ -39,4 +43,5 @@ require (
 	golang.org/x/text v0.23.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/service/order_test.go
@@ -0,0 +1,107 @@
+package service_test
+
+import (
+	"OrderService/internal/entities"
+	"OrderService/internal/service"
+	mocks "OrderService/internal/service/mocks"
+	txMocks "OrderService/pkg/trm/mocks"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestOrderService_SaveOrder(t *testing.T) {
+	type MockBehavior func(orderRepo *mocks.MockOrderRepo)
+
+	dbError := errors.New("db error")
+
+	testCases := []struct {
+		name         string
+		order        entities.Order
+		mockBehavior MockBehavior
+		wantErr      error
+	}{
+		{
+			name: "OK",
+			order: entities.Order{
+				OrderUID: "123",
+			},
+			mockBehavior: func(orderRepo *mocks.MockOrderRepo) {
+				orderRepo.EXPECT().SaveOrder(mock.Anything, mock.Anything).Return(nil)
+				orderRepo.EXPECT().SaveDelivery(mock.Anything, mock.Anything, mock.Anything).Return(nil)
+				orderRepo.EXPECT().SavePayment(mock.Anything, mock.Anything, mock.Anything).Return(nil)
+				orderRepo.EXPECT().SaveItems(mock.Anything, mock.Anything, mock.Anything).Return(nil)
+			},
+			wantErr: nil,
+		},
+		{
+			name:  "SaveOrder fails",
+			order: entities.Order{OrderUID: "123"},
+			mockBehavior: func(orderRepo *mocks.MockOrderRepo) {
+				orderRepo.EXPECT().SaveOrder(mock.Anything, mock.Anything).
+					Return(dbError)
+			},
+			wantErr: dbError,
+		},
+		{
+			name:  "SaveDelivery fails",
+			order: entities.Order{OrderUID: "123"},
+			mockBehavior: func(orderRepo *mocks.MockOrderRepo) {
+				orderRepo.EXPECT().SaveOrder(mock.Anything, mock.Anything).Return(nil)
+				orderRepo.EXPECT().SaveDelivery(mock.Anything, mock.Anything, mock.Anything).
+					Return(dbError)
+			},
+			wantErr: dbError,
+		},
+		{
+			name:  "Retry works (first attempt fails, second succeeds)",
+			order: entities.Order{OrderUID: "123"},
+			mockBehavior: func(orderRepo *mocks.MockOrderRepo) {
+				// первая попытка - SaveOrder падает
+				orderRepo.EXPECT().SaveOrder(mock.Anything, mock.Anything).
+					Once().Return(fmt.Errorf("temporary error"))
+				// вторая попытка - всё ок
+				orderRepo.EXPECT().SaveOrder(mock.Anything, mock.Anything).
+					Once().Return(nil)
+				orderRepo.EXPECT().SaveDelivery(mock.Anything, mock.Anything, mock.Anything).Return(nil)
+				orderRepo.EXPECT().SavePayment(mock.Anything, mock.Anything, mock.Anything).Return(nil)
+				orderRepo.EXPECT().SaveItems(mock.Anything, mock.Anything, mock.Anything).Return(nil)
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			orderRepo := mocks.NewMockOrderRepo(t)
+			tx := txMocks.NewMockManager(t)
+			logger := slog.New(slog.NewTextHandler(io.Discard, nil))
+
+			tx.EXPECT().
+				Do(mock.Anything, mock.Anything).
+				RunAndReturn(
+					func(ctx context.Context, cb func(ctx context.Context) error) error {
+						return cb(ctx)
+					})
+
+			tc.mockBehavior(orderRepo)
+
+			svc := service.NewOrderService(logger, tx, orderRepo)
+
+			err := svc.SaveOrder(context.Background(), tc.order)
+
+			if tc.wantErr != nil {
+				assert.ErrorIs(t, err, tc.wantErr)
+				return
+			}
+
+			assert.NoError(t, err)
+		})
+	}
+}
